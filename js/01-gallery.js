c83382721b8716456e08ebeaf832901bda1d741b
Andrii Kulyk <andrij.kulyk@gmail.com>
2022-07-20 11:35:58 +0300

    Done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryItemsArray = galleryItems
+  .map(
+    el => `<div class="gallery__item"><a class="gallery__link" href="${el.original}">
+        <img class="gallery__image" data-source="${el.original}" src="${el.preview}" alt="${el.description}"></a></div>`
+  )
+  .join('');
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemsArray);
+
+galleryContainer.addEventListener('click', onImgModal);
+
+function onImgModal(event) {
+  event.preventDefault();
+
+  if (event.target.classList.contains('gallery__image')) {
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+    instance.show();
+
+    addEventListener('keydown', escClose);
+
+    function escClose(event) {
+      if (event.code === 'Escape') {
+        instance.close();
+      }
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryItemsArray = galleryItems
+  .map(
+    el => `<a class="gallery__item" href="${el.original}">
+  <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+</a>`
+  )
+  .join('');
+console.log(galleryItemsArray);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemsArray);
+
+galleryContainer.addEventListener('click', onSliderOpen);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
+
+function onSliderOpen(event) {
+  if (event.target.classList.contains('gallery__item')) {
+    lightbox.open();
+  }
+}
